a4553ad386eb5b56b071830783f448cfee030ee1
Jeff Gorder <jeffgorder@gmail.com>
2012-08-23 08:39:08 -0400

    Initial commit

--- app.js
+++ app.js
@@ -26,28 +26,7 @@ app.http.before = [
 // app.router is now available. app[HTTP-VERB] is also available
 // as a shortcut for creating routes
 
-app.router.get('unsubscribe/:id', function(){
-  //console.log('req', path.basename(this.req.url));
-  var res = this.res;
-  var id = path.basename(this.req.url);
-  Common.unsubscribe(id, function(err, reply){
-    Common.getUserApps([id], function(err, app){
-      //console.log('userApp', app);
-      
-      var map = Plates.Map();
-      map.where('href').is('/').insert('newurl');
-      var data = {newurl: 'http://'+settings.url + '/subscribe/'+ id };
-      //console.log("subscribe data", data);
-      var html = Common.render(Common.getHtml('unsubscribe-success','html'), data, map);
-      res.write(html);
-      res.end();
-    });
-    
-    
-  });
-});
-
-app.router.get('subscribe/:id', function(){
+app.router.get('demo/:id', function(){
   //console.log('req', path.basename(this.req.url));
   var res = this.res;
   var id = path.basename(this.req.url);
@@ -58,27 +37,6 @@ app.router.get('subscribe/:id', function(){
       res.end();
     });
   });
-});
-
-app.router.get('/features', function() {
-  var features = [];
-  var tips = [];
-  var nls = JSON.parse(fs.readFileSync( __dirname +'/lib/nls/nls-alert.json', 'utf8'));
-  //randomize the features for updates
-  for (var key in nls.features.productivity) {
-    var source = nls.features.productivity[key];
-    var tip = {};
-    tip.feature = source.feature;
-    tip.featureHeadline = source.headline;
-    tip.featureDescription = source.description;
-    tips.push(tip);
-  };
-  
-  this.res.writeHead(200, { 'Content-Type': 'text/html' });
-  var list = Common.render(Common.getHtml('feature','html'),{tips: tips});
-  var html = Common.render(Common.getHtml('feature-list','html'), {features: list});
-  this.res.write(html);
-  this.res.end();
 });
 
 //Get the the update sequence of the dbase and start following changes
